52f349f68de1b7179025e4425c35911ede5cea5f
David Ordovskiy <david.ordovsky@gmail.com>
2017-07-09 17:40:47 -0400

    add edit/update actions for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,6 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio_item, only: [:show, :edit, :update, :destroy]
+
   def index
     @portfolio_items = Portfolio.all
   end
@@ -19,10 +21,25 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  def edit
+  end
 
+  def update
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_item_params)
+        format.html { redirect_to portfolios_path, notice: "Your portfolio item was successfully updated! "}
+      else
+        format.html { render :edit }
+      end
+    end
+  end
 
   private
     def portfolio_item_params
       params.require(:portfolio).permit(:title, :subtitle, :body)
+    end
+
+    def set_portfolio_item
+      @portfolio_item = Portfolio.find(params[:id])
     end
 end
