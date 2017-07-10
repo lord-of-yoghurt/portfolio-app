32fa9a12dcbc843a1ac3acb73aea92809c4865af
David Ordovskiy <david.ordovsky@gmail.com>
2017-07-10 18:56:23 -0400

    add draft/published status to blogs using an enum

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,8 @@
 class Blog < ApplicationRecord
+  # default state of a post is draft
+  enum status: { draft: 0, published: 1 }
+
+  # to turn title into slug for show route
   extend FriendlyId
   friendly_id :title, use: :slugged
 end
