09df332426c4eea86475eecc37fc677b6ef1cdfa
David Ordovskiy <david.ordovsky@gmail.com>
2017-07-17 22:34:02 -0400

    nest technologies attributes under portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -18,6 +18,7 @@ class PortfoliosController < ApplicationController
 
   def new
     @portfolio_item = Portfolio.new
+    3.times { @portfolio_item.technologies.build }
   end
 
   def create
@@ -55,7 +56,12 @@ class PortfoliosController < ApplicationController
 
   private
     def portfolio_item_params
-      params.require(:portfolio).permit(:title, :subtitle, :body)
+      params.require(:portfolio).permit(
+        :title,
+        :subtitle,
+        :body,
+        technologies_attributes: [:name]
+      )
     end
 
     def set_portfolio_item
