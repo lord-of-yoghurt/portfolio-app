a84ed20c85dc8d1bb804d8fb4f0bf9cfdb3add04
David Ordovskiy <david.ordovsky@gmail.com>
2017-08-27 19:27:16 -0400

    update file upload form, refactor portfolio img placeholder

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,11 +1,7 @@
 class Portfolio < ApplicationRecord
-  include Placeholder
-
   has_many :technologies, dependent: :destroy
   accepts_nested_attributes_for :technologies,
                                 reject_if: lambda { |attrs| attrs['name'].blank? }
-
-  validates_presence_of :title, :body, :main_image, :thumb_image
 
   mount_uploader :thumb_image, PortfolioUploader
   mount_uploader :main_image, PortfolioUploader
@@ -15,12 +11,5 @@ class Portfolio < ApplicationRecord
 
   def self.by_position
     order("position ASC")
-  end
-
-  after_initialize :set_defaults
-
-  def set_defaults
-    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
-    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
   end
 end
