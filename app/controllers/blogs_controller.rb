eb37850213216e93fabe27b1387c45d75b1cf3df
David Ordovskiy <david.ordovsky@gmail.com>
2017-09-08 12:37:24 -0400

    update scope for blogs to hide drafts and sort blogs by most recent

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -11,7 +11,12 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.page(params[:page]).per(5)
+    if logged_in?(:site_admin)
+      @blogs = Blog.recent.page(params[:page]).per(5)
+    else
+      @blogs = Blog.published.recent.page(params[:page]).per(5)
+    end
+
     @page_title = "Blogs"
   end
 
@@ -20,7 +25,7 @@ class BlogsController < ApplicationController
   def show
     @blog = Blog.includes(:comments).friendly.find(params[:id])
     @comment = Comment.new
-    
+
     @page_title = "View Blog | #{@blog.title}"
   end
 
