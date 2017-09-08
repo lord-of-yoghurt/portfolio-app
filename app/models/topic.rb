589c70dfbcaff7ae2249d02750b2c0e5517ee8a2
David Ordovskiy <david.ordovsky@gmail.com>
2017-09-08 19:24:19 -0400

    create topics sidebar widget with custom scope, change sidebar styles

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,4 +2,10 @@ class Topic < ApplicationRecord
   validates_presence_of :title
 
   has_many :blogs
+
+  # select only topics that have blogs attached to them,
+  # i.e. topics that were used for a blog at least once
+  def self.with_blogs
+    includes(:blogs).where.not(blogs: { id: nil })
+  end
 end
